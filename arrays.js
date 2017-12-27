df5634fb13ffd6bce3528f0ffe807de0fae61da1
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-27 01:42:24 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,9 +1,8 @@
 let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
 function addElementToBeginningOfArray (arr, ele) {
-  arr.unshift(ele);
-  return arr;
-};
+  return [ele, ...arr];
+}
 
 const destructivelyAddElementToBeginningOfArray = (arr, ele) => {
   var newArr = [ele, ...arr];
