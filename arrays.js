3667df47c425bcc3d10719933116c3f49c9a3c65
asmaa.abbas@wework.com <asmaa.abbas@wework.com>
2017-12-27 01:43:12 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,4 +1,4 @@
-let chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
 function addElementToBeginningOfArray (arr, ele) {
   return [ele, ...arr];
